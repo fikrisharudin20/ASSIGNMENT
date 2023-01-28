599db50537fd0f180d66d0b5f71a3a4d6dcb2202
fikrisharudin02 <1221302093@student.mmu.edu.my>
2023-01-28 15:20:32 +0800

    add files

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// ********************************************************* 
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS 
+// Year: Trimester 1, 2022/23 (T2215) 
+// Lab: TT1L 
+// Names: AHMAD FIKRI BIN SHARUDIN | MEMBER_NAME_2 | MEMBER_NAME_3 
+// IDs: 1221302093 | MEMBER_ID_2 | MEMBER_ID_3 
+// Emails: 1221302093@student.mmu.edu.my | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 
+// Phones: 0197581622 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
+// ********************************************************* 
+
+// TODO: Fill in the missing information above and delete this line.
+
+#include "pf/helper.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Assignment (Part 1)" << endl;
+    cout << "Let's Get Started!" << endl;
+    pf::Pause();
+}
